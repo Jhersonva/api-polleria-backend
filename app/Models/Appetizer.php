b2e068c5e6fcf3654ee0c3540ec0c8a8616a0aff
josephgonzales2025 <josephacuario20@gmail.com>
2025-05-23 17:56:46 -0500

    Primer commit de modelos y migraciones

---
+++ app/Models/Appetizer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Appetizer extends Model
+{
+    protected $table = 'appetizers';
+    protected $fillable = ['name'];
+}
